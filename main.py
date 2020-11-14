92dadcb7f95948ceb85bdaeba152c2b4421975c1
Stefan Hall <easystef@users.noreply.github.com>
2020-11-14 20:26:41 +0100

    Graphs per country added for daily cases and deaths; risikogebiete.py added to scape information from rki.de

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import argparse
 from bokeh.layouts import layout
-from bokeh.models import HoverTool, NumeralTickFormatter
+from bokeh.models import HoverTool
 from bokeh.models.annotations import Span
 from bokeh.palettes import Category10
 from bokeh.plotting import figure, output_file, show
@@ -33,8 +33,9 @@ class Country:
 
         # Prepare data
         country_data = covid_data[covid_data['countriesAndTerritories'] == country_name]
-        country_data.set_index('dateRep', inplace=True)
+        country_data.set_index('dateRep', drop=False, inplace=True)
 
+        self.date = country_data['dateRep'].sort_index()
         self.cases = country_data['cases'].sort_index()
         self.deaths = country_data['deaths'].sort_index()
         self.population = country_data['popData2019'][0]
@@ -101,7 +102,7 @@ def main():
     # BOKEH ----------------------------
 
     # output to static HTML file
-    output_file("output/lines.html")
+    output_file("output/covid-graph.html")
 
     colours = Category10[max(len(countries), 3)]  # Category10 does not work with an input of <3
     if len(countries) > len(colours):
@@ -109,29 +110,34 @@ def main():
 
     # 1. Create the figures
 
-    # Graph 1
+    # Graph 1 - Active cases
     hover1 = HoverTool(tooltips=[('date', '$x{%F}'), ('cases', '$y{0,0}k')], formatters={'$x': 'datetime'})
-    p1 = figure(width=1200, height=400, title="Active cases", tools=[hover1], x_axis_type="datetime",
+    p1 = figure(width=1200, height=300, title="Active cases", tools=[hover1], x_axis_type="datetime",
                 x_axis_label='date', y_axis_label="thousands of cases")
     p1.xaxis.formatter.months = '%b-%y'
 
-    # Graph 2
+    # Graph 2 - Cases in previous week per 100k people
     hover2 = HoverTool(tooltips=[('date', '$x{%F}'), ('cases', '$y{0,0}')], formatters={'$x': 'datetime'})
-    p2 = figure(width=600, height=400, title="Cases in previous week per 100k people", tools=[hover2],
+    p2 = figure(width=600, height=300, title="Cases in previous week per 100k people", tools=[hover2],
                 x_axis_type="datetime", x_axis_label='date', y_axis_label='cases')
     p2.xaxis.formatter.days = '%d-%b'
 
-    # Graph 3
+    # Graph 3 - R-Number
     hover3 = HoverTool(tooltips=[('date', '$x{%F}'), ('r-number', '$y')], formatters={'$x': 'datetime'})
-    p3 = figure(width=600, height=400, title="R-Number", tools=[hover3], x_axis_type="datetime", x_axis_label='date',
+    p3 = figure(width=600, height=300, title="R-Number", tools=[hover3], x_axis_type="datetime", x_axis_label='date',
                 y_axis_label='r-number')
     p3.xaxis.formatter.days = '%d-%b'
+
+    # Create lists to contain country specific graphs for number of cases per day and number of deaths per day
+    cases = []
+    deaths = []
 
     # 2. Create glyphs
 
     for i, country in enumerate(countries):
         my_country = Country(data, country)
 
+        # Plot graphs 1 - 3
         s1 = my_country.active_cases() / 1000
         p1.line(s1.index, s1.values, legend_label=country, line_width=2, line_color=colours[i])
 
@@ -140,6 +146,21 @@ def main():
         
         s3 = my_country.r_number(4, 7)[-60:]
         p3.line(s3.index, s3.values, legend_label=country, line_width=2, line_color=colours[i])
+
+        # Graph 4 - Cases
+        p4 = figure(width=600, height=200, title=f'{country} - Cases per Day', x_axis_type="datetime",
+                    x_axis_label='date', y_axis_label='cases')
+        p4.vbar(my_country.date, top=my_country.cases, color=colours[i])
+        p4.xaxis.formatter.months = '%b-%y'
+
+        # Graph 5 - Deaths
+        p5 = figure(width=600, height=200, title=f'{country} - Deaths per Day', x_axis_type="datetime",
+                    x_axis_label='date', y_axis_label='deaths')
+        p5.vbar(my_country.date, top=my_country.deaths, color=colours[i])
+        p5.xaxis.formatter.months = '%b-%y'
+
+        cases.append(p4)
+        deaths.append(p5)
 
     for p in [p1, p2, p3]:
         p.legend.location = 'top_left'
@@ -150,7 +171,8 @@ def main():
     # 3. Show the results
     show(layout([
         [p1],
-        [p2, p3]
+        [p2, p3],
+        [cases, deaths]
     ]))
 
 
---
+++ risikogebiete.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+import requests
+
+RKI_URL = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Risikogebiete_neu.html'
+
+
+def main():
+
+    rki = requests.get(RKI_URL)
+    soup = BeautifulSoup(rki.text, 'html.parser')
+
+    main_div = soup.find('div', {'id': 'main'})
+
+    countries = []
+
+    line_item = main_div.ul.li
+
+    while True:
+
+        if sub_items := line_item.findAll('li'):
+            x = line_item.p.string
+            for subentry in sub_items:
+                x += f' {subentry.string};'
+            countries.append(x)
+        else:
+            countries.append(line_item.string)
+
+        if line_item.nextSibling:
+            line_item = line_item.nextSibling
+        else:
+            break
+
+    for i in countries:
+        a, b = unpack_country(i)
+        #b = parse_date(b)
+        print(i)
+        print((a, b))
+
+
+def unpack_country(line_item):
+
+    x1 = line_item.find('(seit')
+    d1 = line_item.find('(seit') + 6  # Corresponds to '(seit '
+    d2 = line_item.find(')')
+
+    country = line_item[:x1]
+
+    risk_since = line_item[d1:d2]
+
+    return country, risk_since
+
+
+def parse_date(date):
+
+    month_dict = {'Januar': '01', 'Februar': '02', 'März': '03', 'April': '04', 'Mai': '05', 'Juni': '06', 'Juli': '07',
+                  'August': '08', 'September': '09', 'Oktober': '10', 'November': '11', 'Dezember': '12'}
+
+    x = date.find('.')
+
+    return '2020-' + month_dict[date[x+2:]] + '-' + date[:x]
+
+
+if __name__ == '__main__':
+    main()
+
+
+# TODO split country name from bit in parenthesis
+# TODO translate
