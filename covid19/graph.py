988f310c332f055ad81cccbb6af89954df2e9919
Stefan Hall <easystef@users.noreply.github.com>
2021-01-11 12:58:59 +0100

    Graphs added for deaths per 100k people and total vaccinations per 100 people.

--- covid19/graph.py
+++ covid19/graph.py
@@ -37,6 +37,7 @@ class Country:
         self.date = country_data['date'].sort_index()
         self.cases = country_data['new_cases'].sort_index()
         self.deaths = country_data['new_deaths'].sort_index()
+        self.vaccinations = country_data['new_vaccinations'].sort_index()
         self.population = country_data['population'][0]
 
     def r_number(self, lag=1, n_days=1):
@@ -82,6 +83,24 @@ class Country:
 
         return self.cases.rolling(7).sum() / (self.population / 100000)
 
+    def deaths_by_population(self):
+        """The number of cases over the last 7 days per 100k people.
+
+        :return: pandas.Series
+            Resulting number of deaths
+        """
+
+        return self.deaths.rolling(7).sum() / (self.population / 100000)
+
+    def total_vaccinations_per_hundred(self):
+        """The cumulative number of vaccinations per 100 people in the population
+
+        :return: pandas.Series
+            Resulting number lof vaccinations
+        """
+
+        return self.vaccinations.cumsum() / (self.population / 100)
+
 
 def make_graphs(data, countries, file_name):
     """
@@ -104,24 +123,43 @@ def make_graphs(data, countries, file_name):
     # 1. Create the figures
 
     # Graph 1 - Active cases
-    hover1 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('cases', '$y{0,0}k')], formatters={'$x': 'datetime'})
+    hover1 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('cases', '$y{0,0}k')],
+                       formatters={'$x': 'datetime'})
     p1 = figure(width=1200, height=300, title="Active cases", tools=[hover1], x_axis_type="datetime",
                 x_axis_label='date', y_axis_label="thousands of cases")
     p1.xaxis.formatter.months = '%b-%y'
     p1.y_range.start = 0
 
     # Graph 2 - Cases in previous week per 100k people
-    hover2 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('cases', '$y{0,0}')], formatters={'$x': 'datetime'})
+    hover2 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('cases', '$y{0,0}')],
+                       formatters={'$x': 'datetime'})
     p2 = figure(width=600, height=300, title="Cases in previous week per 100k people", tools=[hover2],
                 x_axis_type="datetime", x_axis_label='date', y_axis_label='cases')
     p2.xaxis.formatter.days = '%d-%b'
     p2.y_range.start = 0
 
     # Graph 3 - R-Number
-    hover3 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('r-number', '$y')], formatters={'$x': 'datetime'})
+    hover3 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('r-number', '$y')],
+                       formatters={'$x': 'datetime'})
     p3 = figure(width=600, height=300, title="R-Number", tools=[hover3], x_axis_type="datetime", x_axis_label='date',
                 y_axis_label='r-number')
     p3.xaxis.formatter.days = '%d-%b'
+
+    # Graph 4 - Deaths in previous week per 100k people
+    hover4 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('deaths', '$y{0,0}')],
+                       formatters={'$x': 'datetime'})
+    p4 = figure(width=600, height=300, title="Deaths in previous week per 100k people", tools=[hover4],
+                x_axis_type="datetime", x_axis_label='date', y_axis_label='deaths')
+    p4.xaxis.formatter.days = '%d-%b'
+    p4.y_range.start = 0
+
+    # Graph 5 - Total vaccinations per 100 people
+    hover5 = HoverTool(tooltips=[('country', '$name'), ('date', '$x{%F}'), ('vaccinations', '$y{0,0}')],
+                       formatters={'$x': 'datetime'})
+    p5 = figure(width=600, height=300, title="Total vaccinations per 100 people", tools=[hover5],
+                x_axis_type="datetime", x_axis_label='date', y_axis_label='vaccinations')
+    p5.xaxis.formatter.days = '%d-%b'
+    p5.y_range.start = 0
 
     # Create lists to contain country specific graphs for number of cases per day and number of deaths per day
     cases = []
@@ -132,7 +170,7 @@ def make_graphs(data, countries, file_name):
     for i, country in enumerate(countries):
         my_country = Country(data, country)
 
-        # Plot graphs 1 - 3
+        # Plot graphs 1 - 4
         s1 = my_country.active_cases() / 1000
         p1.line(s1.index, s1.values, name=country, legend_label=country, line_width=2, line_color=colours[i])
 
@@ -142,22 +180,28 @@ def make_graphs(data, countries, file_name):
         s3 = my_country.r_number(4, 7)[-60:]
         p3.line(s3.index, s3.values, name=country, legend_label=country, line_width=2, line_color=colours[i])
 
-        # Graph 4 - Cases
-        p4 = figure(width=600, height=200, title=f'{country} - Cases per Day', x_axis_type="datetime",
+        s4 = my_country.deaths_by_population()[-60:]
+        p4.line(s4.index, s4.values, name=country, legend_label=country, line_width=2, line_color=colours[i])
+
+        s5 = my_country.total_vaccinations_per_hundred()[-60:]
+        p5.line(s5.index, s5.values, name=country, legend_label=country, line_width=2, line_color=colours[i])
+
+        # Graph 6 - Cases
+        p6 = figure(width=600, height=200, title=f'{country} - Cases per Day', x_axis_type="datetime",
                     x_axis_label='date', y_axis_label='cases')
-        p4.vbar(my_country.date, top=my_country.cases, color=colours[i])
-        p4.xaxis.formatter.months = '%b-%y'
+        p6.vbar(my_country.date, top=my_country.cases, color=colours[i])
+        p6.xaxis.formatter.months = '%b-%y'
 
-        # Graph 5 - Deaths
-        p5 = figure(width=600, height=200, title=f'{country} - Deaths per Day', x_axis_type="datetime",
+        # Graph 7 - Deaths
+        p7 = figure(width=600, height=200, title=f'{country} - Deaths per Day', x_axis_type="datetime",
                     x_axis_label='date', y_axis_label='deaths')
-        p5.vbar(my_country.date, top=my_country.deaths, color=colours[i])
-        p5.xaxis.formatter.months = '%b-%y'
+        p7.vbar(my_country.date, top=my_country.deaths, color=colours[i])
+        p7.xaxis.formatter.months = '%b-%y'
 
-        cases.append(p4)
-        deaths.append(p5)
+        cases.append(p6)
+        deaths.append(p7)
 
-    for p in [p1, p2, p3]:
+    for p in [p1, p2, p3, p4, p5]:
         p.legend.location = 'top_left'
 
     r_one = Span(location=1, dimension='width', line_color='maroon', line_width=2)
@@ -167,6 +211,7 @@ def make_graphs(data, countries, file_name):
     show(layout([
         [p1],
         [p2, p3],
+        [p4, p5],
         [cases, deaths]
     ]))
 
